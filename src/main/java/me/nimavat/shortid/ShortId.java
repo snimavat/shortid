1f73bb8b4c6f58a8efb7788a06c28cef632834fa
sudhir nimavat <sudhir@nimavat.me>
2017-03-06 15:08:21 +0530

    Add helpers as per the original Shortid implementation

--- src/main/java/me/nimavat/shortid/ShortId.java
+++ src/main/java/me/nimavat/shortid/ShortId.java
@@ -4,10 +4,44 @@ public class ShortId {
 
 	private static final int version = 6;
 	private static final Long REDUCE_TIME = 1459707606518L;
-	private static final int clusterWorkerId = 0;
+	private static int clusterWorkerId = 0;
 
 	private static int counter;
 	private static int previousSeconds;
+
+
+	/**
+	 * Set the seed.
+	 * Highly recommended if you don't want people to try to figure out your id schema.
+	 * exposed as shortid.seed(int)
+	 * @param seed Integer value to seed the random alphabet.  ALWAYS USE THE SAME SEED or you might get overlaps.
+	 */
+	public static void seed(Long seedValue) {
+		Alphabet.setSeed(seedValue);
+	}
+
+	/**
+	 * Set the cluster worker or machine id
+	 * exposed as shortid.worker(int)
+	 * @param workerId worker must be positive integer.  Number less than 16 is recommended.
+	 * returns shortid module so it can be chained.
+	 */
+	public static void worker(int workerId) {
+		clusterWorkerId = workerId;
+	}
+
+	/**
+	 *
+	 * sets new characters to use in the alphabet
+	 * returns the shuffled alphabet
+	 */
+	public static String characters(String newCharacters) {
+		if (newCharacters != null) {
+			Alphabet.characters(newCharacters);
+		}
+
+		return Alphabet.getShuffled();
+	}
 
 	/**
 	 * Generate unique id
@@ -35,4 +69,5 @@ public class ShortId {
 
 		return str;
 	}
+
 }
